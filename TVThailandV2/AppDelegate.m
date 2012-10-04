ca9043be73d311fbdbaef7754fc4016af7c8cd6c
LuciferUltraM <luciferultram@gmail.com>
2012-10-05 06:38:18 +0700

    - Change Register Device - Release Version 2.2

--- TVThailandV2/AppDelegate.m
+++ TVThailandV2/AppDelegate.m
@@ -261,73 +261,74 @@ static const NSInteger kGANDispatchPeriodSec = 10;
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken
 {
     //    NSLog(@"My token is: %@", deviceToken );
-//    NSOperationQueue *queue = [NSOperationQueue new];
-//    NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(sendDeviceToken:) object:deviceToken];
-//    [queue addOperation:operation];
+    NSOperationQueue *queue = [NSOperationQueue new];
+    NSInvocationOperation *operation = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(sendDeviceToken:) object:devToken];
+    [queue addOperation:operation];
     
-#if !TARGET_IPHONE_SIMULATOR
-    
-	// Get Bundle Info for Remote Registration (handy if you have more than one app)
-	NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
-	NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
-	
-	// Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
-	NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
-	
-	// Set the defaults to disabled unless we find otherwise...
-	NSString *pushBadge = (rntypes & UIRemoteNotificationTypeBadge) ? @"enabled" : @"disabled";
-	NSString *pushAlert = (rntypes & UIRemoteNotificationTypeAlert) ? @"enabled" : @"disabled";
-	NSString *pushSound = (rntypes & UIRemoteNotificationTypeSound) ? @"enabled" : @"disabled";
-	
-	// Get the users Device Model, Display Name, Unique ID, Token & Version Number
-	UIDevice *dev = [UIDevice currentDevice];
-	NSString *deviceUuid;
-	if ([dev respondsToSelector:@selector(uniqueIdentifier)])
-		deviceUuid = dev.uniqueIdentifier;
-	else {
-		NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-		id uuid = [defaults objectForKey:@"deviceUuid"];
-		if (uuid)
-			deviceUuid = (NSString *)uuid;
-		else {
-			CFStringRef cfUuid = CFUUIDCreateString(NULL, CFUUIDCreate(NULL));
-			deviceUuid = (__bridge NSString *)cfUuid;
-			CFRelease(cfUuid);
-			[defaults setObject:deviceUuid forKey:@"deviceUuid"];
-		}
-	}
-	NSString *deviceName = dev.name;
-	NSString *deviceModel = dev.model;
-	NSString *deviceSystemVersion = dev.systemVersion;
-	
-	// Prepare the Device Token for Registration (remove spaces and < >)
-	NSString *deviceToken = [[[[devToken description]
-                               stringByReplacingOccurrencesOfString:@"<"withString:@""]
-                              stringByReplacingOccurrencesOfString:@">" withString:@""]
-                             stringByReplacingOccurrencesOfString: @" " withString: @""];
-	
-	// Build URL String for Registration
-	// !!! CHANGE "www.mywebsite.com" TO YOUR WEBSITE. Leave out the http://
-	// !!! SAMPLE: "secure.awesomeapp.com"
-	NSString *host = kApiDomain;
-	
-	// !!! CHANGE "/apns.php?" TO THE PATH TO WHERE apns.php IS INSTALLED
-	// !!! ( MUST START WITH / AND END WITH ? ).
-	// !!! SAMPLE: "/path/to/apns.php?"
-	NSString *urlString = [NSString stringWithFormat:@"/apns?task=%@&appname=%@&appversion=%@&deviceuid=%@&devicetoken=%@&devicename=%@&devicemodel=%@&deviceversion=%@&pushbadge=%@&pushalert=%@&pushsound=%@", @"register", appName,appVersion, deviceUuid, deviceToken, deviceName, deviceModel, deviceSystemVersion, pushBadge, pushAlert, pushSound];
-	
-	// Register the Device Data
-	// !!! CHANGE "http" TO "https" IF YOU ARE USING HTTPS PROTOCOL
-	NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:[urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-	NSLog(@"Register URL: %@", url);
-	NSLog(@"Return Data: %@", returnData);
-	
-#endif
+//#if !TARGET_IPHONE_SIMULATOR
+//    
+//	// Get Bundle Info for Remote Registration (handy if you have more than one app)
+//	NSString *appName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
+//	NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+//	
+//	// Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
+//	NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
+//	
+//	// Set the defaults to disabled unless we find otherwise...
+//	NSString *pushBadge = (rntypes & UIRemoteNotificationTypeBadge) ? @"enabled" : @"disabled";
+//	NSString *pushAlert = (rntypes & UIRemoteNotificationTypeAlert) ? @"enabled" : @"disabled";
+//	NSString *pushSound = (rntypes & UIRemoteNotificationTypeSound) ? @"enabled" : @"disabled";
+//	
+//	// Get the users Device Model, Display Name, Unique ID, Token & Version Number
+//	UIDevice *dev = [UIDevice currentDevice];
+//	NSString *deviceUuid;
+//	if ([dev respondsToSelector:@selector(uniqueIdentifier)])
+//		deviceUuid = dev.uniqueIdentifier;
+//	else {
+//		NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+//		id uuid = [defaults objectForKey:@"deviceUuid"];
+//		if (uuid)
+//			deviceUuid = (NSString *)uuid;
+//		else {
+//			CFStringRef cfUuid = CFUUIDCreateString(NULL, CFUUIDCreate(NULL));
+//			deviceUuid = (__bridge NSString *)cfUuid;
+//			CFRelease(cfUuid);
+//			[defaults setObject:deviceUuid forKey:@"deviceUuid"];
+//		}
+//	}
+//	NSString *deviceName = dev.name;
+//	NSString *deviceModel = dev.model;
+//	NSString *deviceSystemVersion = dev.systemVersion;
+//	
+//	// Prepare the Device Token for Registration (remove spaces and < >)
+//	NSString *deviceToken = [[[[devToken description]
+//                               stringByReplacingOccurrencesOfString:@"<"withString:@""]
+//                              stringByReplacingOccurrencesOfString:@">" withString:@""]
+//                             stringByReplacingOccurrencesOfString: @" " withString: @""];
+//	
+//	// Build URL String for Registration
+//	// !!! CHANGE "www.mywebsite.com" TO YOUR WEBSITE. Leave out the http://
+//	// !!! SAMPLE: "secure.awesomeapp.com"
+//	NSString *host = kApiDomain;
+//	
+//	// !!! CHANGE "/apns.php?" TO THE PATH TO WHERE apns.php IS INSTALLED
+//	// !!! ( MUST START WITH / AND END WITH ? ).
+//	// !!! SAMPLE: "/path/to/apns.php?"
+//	NSString *urlString = [NSString stringWithFormat:@"/apns?task=%@&appname=%@&appversion=%@&deviceuid=%@&devicetoken=%@&devicename=%@&devicemodel=%@&deviceversion=%@&pushbadge=%@&pushalert=%@&pushsound=%@", @"register", appName,appVersion, deviceUuid, deviceToken, deviceName, deviceModel, deviceSystemVersion, pushBadge, pushAlert, pushSound];
+//	
+//	// Register the Device Data
+//	// !!! CHANGE "http" TO "https" IF YOU ARE USING HTTPS PROTOCOL
+//	NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:[urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+//    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+//	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+//	NSLog(@"Register URL: %@", url);
+//	NSLog(@"Return Data: %@", returnData);
+//	
+//#endif
     
     
 }
+
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
 #if !TARGET_IPHONE_SIMULATOR
@@ -373,19 +374,20 @@ static const NSInteger kGANDispatchPeriodSec = 10;
 
 - (void)sendDeviceToken:(NSData *)deviceToken
 {
+#if !TARGET_IPHONE_SIMULATOR
     NSString *token = [[[[NSString stringWithFormat:@"%@",deviceToken]
                          stringByReplacingOccurrencesOfString:@"<" withString:@""]
                         stringByReplacingOccurrencesOfString:@">" withString:@""]
                        stringByReplacingOccurrencesOfString:@" " withString:@""];
     NSString *uniqueIdentifier = [[UIDevice currentDevice] uniqueIdentifier];
     
-    NSLog(@"uniqueIdentifier : %@",uniqueIdentifier);
-    NSLog(@"token : %@",token);
+//    NSLog(@"uniqueIdentifier : %@",uniqueIdentifier);
+//    NSLog(@"token : %@",token);
     
     ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:
                                [NSURL URLWithString: kDeviceToken(uniqueIdentifier, token)]];
     [request startSynchronous];
-
+#endif
 }
 
 - (void)notificationToSwitchApplication:(NSDictionary *)userInfo
