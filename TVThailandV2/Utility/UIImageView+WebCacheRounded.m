218a17f29966fa8ce74eff1f37ac7fa4d1126131
Nattapong Tonprasert <luciferultram@gmail.com>
2015-09-11 23:56:37 +0700

    Fix UIImageView+WebCacheRounded.m

--- TVThailandV2/Utility/UIImageView+WebCacheRounded.m
+++ TVThailandV2/Utility/UIImageView+WebCacheRounded.m
@@ -8,7 +8,7 @@
 
 #import "UIImageView+WebCacheRounded.h"
 #import "UIImage+RoundedImage.h"
-#import <SDWebImage/SDWebImageManager.h>
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @implementation UIImageView (WebCacheRounded)
 
@@ -16,29 +16,29 @@
     [self setImageURL:imageUrl placeholder:placeholderImage radius:radius toDisk:NO];
 }
 
-- (void)setImageURL:(NSURL *)imageUrl placeholder:(UIImage *)placeholderImage radius:(CGFloat)radius toDisk:(BOOL)toDisk
+- (void)setImageURL:(NSURL *)imageURL placeholder:(UIImage *)placeholderImage radius:(CGFloat)radius toDisk:(BOOL)toDisk
 {
-    [[SDImageCache sharedImageCache] queryDiskCacheForKey:imageUrl.absoluteString done:^(UIImage *image, SDImageCacheType cacheType) {
+    [[SDImageCache sharedImageCache] queryDiskCacheForKey:imageURL.absoluteString done:^(UIImage *image, SDImageCacheType cacheType) {
         if (image) {
             self.image = image;
         }
         else {
             self.image = placeholderImage;
-            
-            [SDWebImageDownloader.sharedDownloader downloadImageWithURL:imageUrl
-                                                                options:0
-                                                               progress:^(NSInteger receivedSize, NSInteger expectedSize)
+            SDWebImageManager *manager = [SDWebImageManager sharedManager];
+            [manager downloadImageWithURL:imageURL
+                                  options:0
+                                 progress:^(NSInteger receivedSize, NSInteger expectedSize)
              {
                  // progression tracking code
              }
-                                                              completed:^(UIImage *image, NSData *data, NSError *error, BOOL finished)
+                                completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL)
              {
                  if (image && finished)
                  {
                      UIImage *newImage = [UIImage roundedRectImageFromImage:image withRadious:radius];
                      
                      [[SDImageCache sharedImageCache] storeImage:newImage
-                                                          forKey:imageUrl.absoluteString];
+                                                          forKey:imageURL.absoluteString];
                      
                      self.image = newImage;
                  }
